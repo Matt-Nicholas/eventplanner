01ac7a211dc33544f81c2414e1d334e8bac4d825
Matt Nicholas <mattnicholas@Alanas-MacBook-Pro.local>
2016-09-09 10:25:57 -0700

    finish the back endfunctions

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,2 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Arrays;
+
+public class Event{
+  private int mTotalCost;
+  private int mNumberOfGuests;
+  private boolean mFood;
+  private boolean mDrinks;
+  private boolean mEntertainment;
+
+  public Event(){
+    mTotalCost = 500;
+  }
+  public int getTotalCost(){
+    return mTotalCost;
+  }
+  public int setNumberOfGuests(int num){
+    mNumberOfGuests = num;
+    return mNumberOfGuests;
+  }
+  public boolean setFood(char food){
+    mFood = food =='y';
+    return mFood;
+  }
+  public boolean setDrinks(char drinks){
+    mDrinks = drinks == 'y';
+    return mDrinks;
+  }
+  public boolean setEntertainment(char entertainment){
+    mEntertainment = entertainment == 'y';
+    return mEntertainment;
+  }
+  public void setTotalCost(){
+    int total = 0;
+    if(mDrinks == true){
+      total += (10 * mNumberOfGuests);
+    }
+    if(mEntertainment == true){
+      total += (15 * mNumberOfGuests);
+    }
+    mTotalCost += total;
+  }
+}
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -4,11 +4,41 @@ import static org.junit.Assert.*;
 
 public class EventTest{
 
+
     @Test
-    public void setTotalCost_calculatesTotalCost_100(){
+    public void setNumberOfGuests_setsNumberOfGuestsAttendingTheEvent_100(){
       Event testEvent = new Event();
       int expectedOutput = 100;
-      expectedOutput.add(1);
-      assertEquals(expectedOutput, testEvent.setTotalCost(1));
+      assertEquals(expectedOutput, testEvent.setNumberOfGuests(100));
+    }
+    @Test
+    public void setDrinks_returnsBoolTrueIfDrinksAreSelected_true(){
+      Event testEvent = new Event();
+      boolean expectedOutput = true;
+      assertEquals(expectedOutput, testEvent.setDrinks('y'));
+    }
+    @Test
+    public void setEntertainment_returnsBoolTrueIfEntertainmentAreSelected_true(){
+      Event testEvent = new Event();
+      boolean expectedOutput = true;
+      assertEquals(expectedOutput, testEvent.setEntertainment('y'));
+    }
+    @Test
+    public void setFood_returnsBoolTrueIfFoodAreSelected_true(){
+      Event testEvent = new Event();
+      boolean expectedOutput = true;
+      assertEquals(expectedOutput, testEvent.setFood('y'));
+    }
+
+    @Test
+    public void getTotalCost_returnsTotalCost_100(){
+      Event testEvent = new Event();
+      int expectedOutput = 3000;
+      testEvent.setNumberOfGuests(100);
+      testEvent.setDrinks('y');
+      testEvent.setEntertainment('y');
+      testEvent.setFood('y');
+      testEvent.setTotalCost();
+      assertEquals(expectedOutput, testEvent.getTotalCost());
     }
 }
